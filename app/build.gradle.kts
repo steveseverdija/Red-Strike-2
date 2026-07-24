9beb12538d9c8a6cb9430717cc2c750f70d74d7c
Steve Severdija <steve.sever@live.ca>
2026-07-24 16:42:54 -0400

    Android 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk = 35
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.aistudio.redstrike.xyp3rq.addbaf"
     minSdk = 24
-    targetSdk = 34
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
 
