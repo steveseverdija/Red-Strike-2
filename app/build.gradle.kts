c335c6dbc364a8476e8d60149e00e088013237d1
Steve Severdija <steve.sever@live.ca>
2026-07-24 16:03:08 -0400

    A53 5g update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk = 36
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "com.aistudio.redstrike.xyp3rq.addbaf"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
 
