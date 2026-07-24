b5242862a9bebfb1ca30005cb85d41718c73699f
Steve Severdija <steve.sever@live.ca>
2026-07-24 16:17:44 -0400

    Sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
   defaultConfig {
     applicationId = "com.aistudio.redstrike.xyp3rq.addbaf"
     minSdk = 24
-    targetSdk = 35
+    targetSdk = 34
     versionCode = 1
     versionName = "1.0"
 
